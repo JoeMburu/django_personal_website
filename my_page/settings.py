51b6643b6a829e522d04c45c2d968b18eb351ea8
Joseph Afonne <josef.afonne@gmail.com>
2025-02-26 16:43:42 +0200

    Configure whitenoise

--- my_page/settings.py
+++ my_page/settings.py
@@ -137,3 +137,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
