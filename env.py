0745554c7dd11451d3671105f8166659dfff96a3
Joseph Afonne <josef.afonne@gmail.com>
2025-10-28 15:09:56 +0200

    Add DEBUG select

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-
+os.environ['DEBUG'] = 'True'
 os.environ['SECRET_KEY'] = 'django-insecure-r!&r16tg7@y6o2@(3fd-i*(!efze3+iw14o1#15g*zpvs(q)gg'
 os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:npg_9nQvo6eFVhcr@ep-billowing-river-agiv28yp-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
--- my_page/settings.py
+++ my_page/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
+print("DEBUG =", DEBUG)
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
